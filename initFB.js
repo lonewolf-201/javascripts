ac726f39bf708e5e0540368fa06910acca087a3b
lonewolf-201 <46050515+lonewolf-201@users.noreply.github.com>
2019-04-30 01:38:30 +0530

    for initialize FireBase

---
+++ initFB.js
@@ -0,0 +1,10 @@
+var config = {
+		apiKey: "AIzaSyAtYcrA3HXFQFEjj_QCOpKeSTaG-r2i_iU",
+		authDomain: "omnipotent-e8f61.firebaseapp.com",
+		databaseURL: "https://omnipotent-e8f61.firebaseio.com",
+		projectId: "omnipotent-e8f61",
+		storageBucket: "omnipotent-e8f61.appspot.com",
+		messagingSenderId: "139122634017"
+	};
+	firebase.initializeApp(config);
+	console.log(initialized);
